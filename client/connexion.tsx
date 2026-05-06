50fad47b9a17d3eed1d28ef2c5e0f5481a3b823b
Hans Siemon <hanssiemon@hanss-air.home>
2026-05-06 11:39:18 +0200

    Création de la structure de la page connexion

---
+++ client/connexion.tsx
@@ -0,0 +1,38 @@
+
+import React from "React"
+
+
+function Connexion() {
+
+
+    return(
+<section>
+
+<section>
+<h2>Se connectez</h2>
+<section>
+    <input type="text" size={5} />
+    <input type="text"size={5} />
+    <button type="button">Conexion</button>
+</section>
+</section>
+
+<section>
+<section>
+<h2>Création d'un compte</h2>
+</section>
+<section>
+<input type="text" size={5}/>
+<input type="text" size={5}/>
+<input type="text" size={5}/>
+<input type="password"  size= {5} maxLength={10}/>
+<button type="button">Créer compte </button>
+</section>
+</section>
+
+</section>
+
+    )
+    
+}
+export default Connexion;
