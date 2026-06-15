751e3f016271b4ba1a24c41c1b4b0487d333a2e4
Hans Siemon <hanssiemon@hanss-air.home>
2026-06-15 18:03:22 +0200

    création du fichier Connexion et de sa structure & creation du fichier Connexion.css

---
+++ client/src/pages/Connexion.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+
+
+function Connexion() {
+
+
+
+
+
+    
+
+
+return(
+<section className="Connexion">
+<section className="Se-Connectez">
+<h2>Se Connectez</h2>
+<label>Email</label>
+<input type="email"/>
+<label>Mot de Passe </label>
+<input type="Mot de Passe"/>
+<input type="button">Bouton</input>
+</section>
+
+
+<section className="Creation-de-Compte">
+<h2>Création d'un compte</h2>
+<label>email</label>
+<input type="email"/>
+<label>Mot de passe</label>
+<input type="password"/>
+<label>Prénom</label>
+<input type="firstname" />
+<label>Nom de Famille</label>
+<input type="lastname"/>
+<input type="button">Bouton</input>
+</section>
+</section>
+
+)
+
+
+
+}
+
+export default Connexion;
