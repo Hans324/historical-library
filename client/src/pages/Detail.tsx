f2e0d85657b45e5c91517435ae50befe5a437c98
Hans Siemon <hanssiemon@hanss-air.home>
2026-06-19 00:00:46 +0200

    creation de la structure du fichier Detail

---
+++ client/src/pages/Detail.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+
+
+
+function Detail () {
+
+
+
+return (
+
+<section className="Detail">
+    <section className="detail-img">
+<img className="couverture"></img>
+</section>
+<section>
+<p className="texte"></p>
+<button type="button" className="button-add">button add Book</button>
+<img className="Image"></img>
+</section>
+
+
+</section>
+
+
+
+
+
+
+)
+
+}
+
+export default Detail;
