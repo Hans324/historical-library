2c43eea51c423345d5b1f0fb04952fcdbc16c07f
Hans Siemon <hanssiemon@hanss-air.home>
2026-06-19 10:09:37 +0200

    Creation de la structure du fichier Livre.tsx

---
+++ client/src/pages/Livre.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+
+function Livre () {
+
+
+
+return(
+<section className="section-Livre">
+
+<section className="Romain">
+
+    <img className="Livre-Romaine"></img>
+    <p>auteur:Pierre Grimal</p>
+</section>
+
+<section className="Perse">
+<img className="Livre-Perse"> </img>
+<p>auteur:Leïla Karimi</p>
+</section>
+
+<section className="Charles">
+<img className="Livre-Charlemange"></img>
+<p> auteur: </p>
+</section>
+
+<section className="Religion">
+<img className="Livre-christianisme"></img>
+<p>auteur:</p>
+</section>
+
+
+</section>
+
+
+
+);
+
+}
+
+
+export default Livre;
